90d14262afce021dab02077b860385f025202a3c
raychenon <raymond.chenon@gmail.com>
2023-01-16 23:29:01 +0100

    Git init

---
+++ src/main/java/com/raychenon/tennisscore/TennisScoreApplication.java
@@ -0,0 +1,13 @@
+package com.raychenon.tennisscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisScoreApplication.class, args);
+	}
+
+}
